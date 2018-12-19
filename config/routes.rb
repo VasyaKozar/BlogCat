210b41bf45deca3ecc422cd358a3a56ad2b0013b
VasyaKozar <vasyakozar13@gmail.com>
2018-12-19 19:37:18 +0200

    8 commit

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get 'userparams/new'
+  get 'userparams/show'
+  get 'userparams/edit'
+  devise_for :users
+  root 'pages#ingex'
+  get 'pages/about'
+  get 'pages/contact_us'
+  get 'pages/privacy_policy'
+  resources :posts do
+    resources :comments
+  end
+ resources :userparams
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
